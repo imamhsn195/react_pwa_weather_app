bc1612f921eeb5848b57093605a334bed13cec03
imamhsn195 <imamhsn195@gmail.com>
2020-09-30 16:44:33 +0600

    weather app done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './app.css';
+import { fetchWeather } from './api/fetchWeather';
+const App = () => {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const search = async(e)=>{
+        if(e.key === 'Enter'){
+            const data = await fetchWeather(query);
+            setWeather(data)
+            setQuery('')
+        }
+    }
+
+    return(
+    <div className="weather-container">
+        <h1>Welcome to React Pwa</h1>
+        <input className="search" placeholder="Search..." 
+        value={query} 
+        onChange={(e) => (setQuery(e.target.value))}
+        onKeyPress={search}
+        />
+        {
+            weather.main && (
+                <div>
+                    <h1 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>({weather.sys.country})</sup>
+                    </h1>
+                    <div>
+                        <h1>{Math.round(weather.main.temp)}<span>&deg;C</span></h1>
+                    </div>
+                    <div>
+                        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <div>{weather.weather[0].description.toUpperCase()}</div>
+                    </div>
+                </div>
+            )
+        }
     </div>
-  );
+    ); 
 }
 
 export default App;
